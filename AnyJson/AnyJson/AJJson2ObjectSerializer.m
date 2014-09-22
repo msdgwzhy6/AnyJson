32bde120c9c2e77f05c7d2cfb4efbeb63fe264e4
CasaTaloyum <casatwy@msn.com>
2014-09-22 22:31:51 +0800

    not ready for further coding

--- AnyJson/AnyJson/AJJson2ObjectSerializer.m
+++ AnyJson/AnyJson/AJJson2ObjectSerializer.m
@@ -7,12 +7,33 @@
 //
 
 #import "AJJson2ObjectSerializer.h"
+#import "AJClassHelper.h"
+#import "AJPropertyDescriptor.h"
 
 @implementation AJJson2ObjectSerializer
 
 #pragma mark - public methods
 + (id)transformJsonObject:(id)jsonObject toTargetObjectClass:(Class)targetClass
 {
+    if (jsonObject == nil || [jsonObject isKindOfClass:[NSNull class]]) {
+        return nil;
+    }
+    
+    AJDataType targetDataType = [AJClassHelper dataTypeOfClass:targetClass];
+    
+    NSDictionary *propertyList = [AJClassHelper reflectProperties:targetClass];
+    for (AJPropertyDescriptor *propertyDescriptor in propertyList) {
+        /*
+         NSNumber -> BOOL, NSInteger, Float, Double, Long, NSNumber, NSDate
+         NSString -> NSString
+         for each NSArray -> object
+         for each NSDictionary -> key:object
+         */
+//        if () {
+//        }
+    }
+    
+    
     return nil;
 }
 
